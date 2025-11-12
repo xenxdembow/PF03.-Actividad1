dc39b93366d36f77681da9e2acadf8c5fd85fa67
Jorge Canales <vegiti7777@gmail.com>
2025-11-12 16:09:11 +0100

    TDD refactor, refactorizo la función y veo que pasa los tests

--- src/actividad1.js
+++ src/actividad1.js
@@ -1,20 +1,12 @@
-function fibonacci(numb) {
-    if (numb < 0 || !Number.isInteger(numb)) {
+function fibonacci(n) {
+    if (n < 0 || !Number.isInteger(n)) {
         throw new RangeError("solo números enteros ≥ 0");
     }
 
-    if (numb === 0) return 0;
-    if (numb === 1) return 1;
+    if (n === 0) return 0;
+    if (n === 1) return 1;
 
-    let min = 0; 
-    let max = 1; 
-
-    for (let i = 2; i <= numb; i++) {
-        let act = min + max;
-        min = max;
-        max = act;
-    }
-    return max;
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 export{
