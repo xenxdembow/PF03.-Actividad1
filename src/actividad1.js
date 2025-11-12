3201867cde9746341eec9c4102207285039b09f4
Jorge Canales <vegiti7777@gmail.com>
2025-11-12 15:36:51 +0100

    Creo la primera función

---
+++ src/actividad1.js
@@ -0,0 +1,18 @@
+function fibonacci(numb) {
+    if (numb < 0 || !Number.isInteger(numb)) {
+        throw new RangeError("solo números enteros ≥ 0");
+    }
+
+    if (numb === 0) return 0;
+    if (numb === 1) return 1;
+
+    let min = 0; 
+    let max = 1; 
+
+    for (let i = 2; i < numb; i++) {
+        let act = min + max;
+        min = max;
+        max = act;
+    }
+    return max;
+}
